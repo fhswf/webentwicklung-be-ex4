3fbcbc17609ce6bd74ff011d8e3991869087143b
Christian Gawron <gawron.christian@fh-swf.de>
2023-04-14 13:17:14 +0000

    wip: solution for exercise 4

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,63 @@
 import express from 'express';
 import DB from './db.js'
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Todo API',
+            version: '1.0.0',
+            description: 'Todo API Dokumentation',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+        components: {
+            schemas: {
+                Todo: {
+                    type: 'object',
+                    properties: {
+                        title: {
+                            type: 'string',
+                        },
+                        due: {
+                            type: 'string',
+                        },
+                        status: {
+                            type: 'integer',
+                        },
+                    },
+                },
+            },
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                }
+            },
+        },
+        security: [{
+            bearerAuth: []
+        }],
+
+    },
+    apis: ['./index.js'],
+};
+
 const PORT = process.env.PORT || 3000;
 
 /** Zentrales Objekt für unsere Express-Applikation */
 const app = express();
+
+/** Middleware für Swagger */
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 /** global instance of our database */
 let db = new DB();
@@ -19,6 +72,20 @@ async function initDB() {
 
 /** Return all todos. 
  *  Be aware that the db methods return promises, so we need to use either `await` or `then` here! 
+ * @swagger
+ * /todos:
+ *  get:
+ *    summary: Gibt alle Todos zurück
+ *    tags: [Todos]
+ *    responses:
+ *      '200':
+ *        description: Eine Liste aller Todos
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Todo'
  */
 app.get('/todos', async (req, res) => {
     let todos = await db.queryAll();
